a585b7dd8b62ab4595dadbf1cedeb20ffd773733
Jesus Miramontes <4751856+JesusMiramontes@users.noreply.github.com>
2024-10-16 17:25:33 -0600

    init commit

---
+++ src/main/java/xyz/miramontes/heartbeatrr/HeartbeatrrApplication.java
@@ -0,0 +1,12 @@
+package xyz.miramontes.heartbeatrr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeartbeatrrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeartbeatrrApplication.class, args);
+    }
+}
---
+++ src/test/java/xyz/miramontes/heartbeatrr/HeartbeatrrApplicationTests.java
@@ -0,0 +1,11 @@
+package xyz.miramontes.heartbeatrr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeartbeatrrApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
