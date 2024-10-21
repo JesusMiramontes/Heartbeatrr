e3a6368f196dd8f793f41a26b1eadaffe1f5cd01
Jesus Miramontes <4751856+JesusMiramontes@users.noreply.github.com>
2024-10-21 03:25:06 -0600

    Move from a list of services to a map that includes service name.

--- src/main/java/xyz/miramontes/heartbeatrr/service/HealthCheckService.java
+++ src/main/java/xyz/miramontes/heartbeatrr/service/HealthCheckService.java
@@ -3,6 +3,7 @@ package xyz.miramontes.heartbeatrr.service;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -12,11 +13,12 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 public class HealthCheckService {
-    @Value("#{'${heartbeatrr.services.urls}'.split(',')}")
-    private List<String> servicesUrls;
+
+    // Inject service URLs from application properties
+    @Value("#{${heartbeatrr.services.urls}}")
+    private Map<String, String> serviceUrls;
 
     private final RetryableHealthCheckService retryableHealthCheckService;
-
     private final DiscordService discordService;
 
     public HealthCheckService(
@@ -26,21 +28,44 @@ public class HealthCheckService {
         this.discordService = discordService;
     }
 
-    // Loop through all services and check each one
+    /**
+     * Checks the health of all registered services. If any service is down, it will notify via
+     * Discord.
+     */
     public void checkAllServices() throws IOException {
         List<String> downServices = new ArrayList<>();
-        for (String service : servicesUrls) {
-            log.info("Checking service at URL: {}", service);
-            ResponseEntity<String> result = retryableHealthCheckService.checkEndpoint(service);
-            log.info("Result for service {}: {}", service, result.getBody());
-            if (result.getStatusCode().isSameCodeAs(HttpStatus.SERVICE_UNAVAILABLE)) {
-                downServices.add(service);
-            }
+
+        // Iterate through all services and check their health status
+        serviceUrls.forEach(
+                (serviceName, serviceUrl) -> {
+                    log.debug("Checking service: {} at URL: {}", serviceName, serviceUrl);
+
+                    ResponseEntity<String> response =
+                            retryableHealthCheckService.checkEndpoint(serviceUrl);
+
+                    log.debug("Response for {}: {}", serviceName, response.getBody());
+
+                    // If the service is unavailable, add it to the list of down services
+                    if (response.getStatusCode() == HttpStatus.SERVICE_UNAVAILABLE) {
+                        downServices.add(serviceName + " (" + serviceUrl + ")");
+                    }
+                });
+
+        // If there are any down services, send a notification
+        if (!downServices.isEmpty()) {
+            sendAlertForDownServices(downServices);
         }
-        if (!downServices.isEmpty()) notify(downServices);
     }
 
-    private void notify(List<String> downServices) throws IOException {
-        discordService.sendDiscordAlert("These services are down: " + downServices.toString());
+    /**
+     * Sends a Discord notification with a list of down services.
+     *
+     * @param downServices List of services that are down.
+     * @throws IOException if the Discord notification fails.
+     */
+    private void sendAlertForDownServices(List<String> downServices) throws IOException {
+        String alertMessage = "The following services are down: " + String.join(", ", downServices);
+        discordService.sendDiscordAlert(alertMessage);
+        log.info("Alert sent for down services: {}", downServices);
     }
 }
