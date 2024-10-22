338eb83ee8c2a67585409376cc72462560328a06
Jesus Miramontes <4751856+JesusMiramontes@users.noreply.github.com>
2024-10-21 23:09:24 -0600

    Update discord webhook placeholder

--- src/test/java/xyz/miramontes/heartbeatrr/HeartbeatrrApplicationTests.java
+++ src/test/java/xyz/miramontes/heartbeatrr/HeartbeatrrApplicationTests.java
@@ -2,9 +2,14 @@ package xyz.miramontes.heartbeatrr;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import xyz.miramontes.heartbeatrr.service.DiscordService;
 
 @SpringBootTest
 class HeartbeatrrApplicationTests {
+
+    @MockBean
+    private DiscordService discordService;
 
     @Test
     void contextLoads() {}
